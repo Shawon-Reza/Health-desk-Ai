f4f20863e61d874fc2a0f96ad5bcd6cdb728ff2f
Shawon Reza <shawon.reza.sr81@gmail.com>
2025-10-26 11:23:19 +0600

    4

--- src/features/AdminDashboard/communication/Communication.jsx
+++ src/features/AdminDashboard/communication/Communication.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FiSearch, FiFilter } from 'react-icons/fi';
 import ChatPanel from './ChatPanel';
-import CreateNewGroupModal from './Communication/CreateNewGroupModal';
+import CreateNewGroupModal from '../communication/CreateNewGroupModal';
 
 
 
